ad38f229f603f1282d1cc1bd62af33de0b7a7b6c
ITZMEXYZ <zyeebanez@gmail.com>
2025-11-06 19:52:47 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Home page
+@app.route('/')
+@app.route('/home')
+def index():
+    index_data = {
+        "message": "GROUP 11",
+        "message_1": "Hello everyone we are the Group 11 and this is our Group Portfolio project in DSA"
+    }
+    return render_template('home.html', index=index_data, active_page='home')
+
+# Projects page
+@app.route('/works', methods=['GET', 'POST'])
+def works():
+    return render_template('works.html', active_page='works')
+
+# Members / Contact page
+@app.route('/contacts')
+def members_contact():
+    people = [
+        {
+            "name": "Mark Christian Abucejo",
+            "image": "static/images/pic_1.png",
+            "links": {
+                "facebook": "https://www.facebook.com/mrkchrstnsbcj",
+                "email": "https://www.facebook.com/mrkchrstnsbcj",
+                "github": "https://www.facebook.com/mrkchrstnsbcj",
+            }
+        },
+        {
+            "name": "Zy Banez",
+            "image": "static/images/zy.jpg",
+            "links": {
+                "facebook": "https://www.facebook.com/zyescote.banez.5",
+                "email": "zyescotebanez@gmail.com",
+                "github": "https://github.com/ITZMEXYZ",
+            }
+        },
+        {
+            "name": "Kyle Isaac Celin",
+            "image": "static/images/pic_2.png",
+            "links": {
+                "facebook": "https://www.facebook.com/cee.the.lin.e",
+                "email": "https://youtube.com/@zybanezz",
+                "github": "https://tiktok.com/@zybanezz",
+            }
+        },
+        {
+            "name": "John Luke Fabillan",
+            "image": "static/images/pic_3.png",
+            "links": {
+                "facebook": "https://facebook.com/zybanezz",
+                "email": "https://youtube.com/@zybanezz",
+                "github": "https://tiktok.com/@zybanezz",
+            }
+        },
+        {
+            "name": "Princess Sophia Manalo",
+            "image": "static/images/pic_4.png",
+            "links": {
+                "facebook": "https://facebook.com/zybanezz",
+                "email": "https://youtube.com/@zybanezz",
+                "github": "https://tiktok.com/@zybanezz",
+            }
+        },
+        {
+            "name": "Isaac Christian Pelingen",
+            "image": "static/images/pic_5.png",
+            "links": {
+                "facebook": "https://facebook.com/zybanezz",
+                "email": "https://youtube.com/@zybanezz",
+                "github": "https://tiktok.com/@zybanezz",
+            }
+        },
+        {
+            "name": "Gian Carlos Tumanan",
+            "image": "static/images/pic_6.png",
+            "links": {
+                "facebook": "https://facebook.com/zybanezz",
+                "email": "https://youtube.com/@zybanezz",
+                "github": "https://tiktok.com/@zybanezz",
+            }
+        }
+    ]
+    return render_template("contacts.html", people=people)
+
+# Queue Visualizer page
+@app.route('/works/queue-visualizer', methods=['GET', 'POST'])
+def queue_visualizer():
+    return render_template('queuevisualizer.html')
+
+# DeQueue Visualizer page
+@app.route('/works/dequeue-visualizer', methods=['GET', 'POST'])
+def dequeue_visualizer():
+    return render_template('dequeuevisualizer.html')
