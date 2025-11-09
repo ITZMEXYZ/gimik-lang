f5cb320d963ef4e5d12b46a67e3bd74678bee0a6
ITZMEXYZ <zyeebanez@gmail.com>
2025-11-09 21:40:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, url_for
 import os
 from modules.queue import Queue
 from modules.dequeue import DeQueue
